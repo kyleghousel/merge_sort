223cf12fe7e3bc1fb18aaca55933d1137c3c966d
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2023-04-24 09:32:47 -0400

    Final

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,14 +1,24 @@
 def merge_sort (array)
   return array if array.length == 1
   #find middle
-  middle = array.each_slice( (array.size/2.0).round )
+  middle = array.length/2.0
+  left = array[0...middle]
+  right = array[middle..-1]
   #call merge using merge_sort as l and r
-  merge (merge_sort(array[0..middle]),merge_sort(array[middle..-1]))
+  merge(merge_sort(left), merge_sort(right))
 end
 
 def merge (l, r)
   merged = []
   #merge halves into one array
-  #compare values, put smaller value at front of array, put larger value at back
+  #compare values, put smaller value at front of array, or put larger value at back
+  until l.empty? || r.empty?
+    if l.first <= r.first
+      merged << l.shift
+    else
+      merged << r.shift
+    end
+  end
   #return the merged, or combined array
+  merged + l + r
 end
